daa3936a5b2e60e11da668935c5a838899c5e487
GrantZheng <zhengyuangrant@126.com>
2015-06-22 20:26:45 +0800

    modify get interface, can get all near position

--- backend/src/location/location.go
+++ backend/src/location/location.go
@@ -8,15 +8,47 @@ import (
 type LocationREST struct {
 }
 
+
+/*
+	return 
+	"errno": 0,
+    "errmsg": " ",
+    "data": [
+        {
+            "Uid": "",
+            "Loc": {
+                "Type": "Point",
+                "Coordinates": [
+                    -73.97,
+                    40.77
+                ]
+            }
+        },
+        {
+            "Uid": "",
+            "Loc": {
+                "Type": "Point",
+                "Coordinates": [
+                    73.88,
+                    -20.78
+                ]
+            }
+        }
+    ]
+}*/
+
 //curl 'localhost:8083/Location/Get?uid=123'
 func (this *LocationREST) Get(c *hera.Context) error {
 	params := c.Params
 	uid := params["uid"]
-	ret := get(uid)
+	longitude, _ 	:= strconv.ParseFloat(params["longitude"], 64)  //jingdu
+	latitude, _	:= strconv.ParseFloat(params["latitude"], 64)   //weidu
+
+	ret := get(uid, longitude, latitude)
 	return c.Success(ret)
 }
 
-//curl 'localhost:8083/Location/Set?uid=123&longitude=&latitude='
+//curl 'localhost:8083/Location/Get?uid=123&longitude=-8&latitude=2'
 func (this *LocationREST) Set(c *hera.Context) error {
 		params 		:= c.Params
 		uid		:= params["uid"]
--- backend/src/location/location_storage.go
+++ backend/src/location/location_storage.go
@@ -1,8 +1,8 @@
 package location
 
 import (
-//	"log"
-	"fmt"
+	"log"
+	"os"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -16,21 +16,27 @@ type Location struct {
 	Loc Pos
 }
 
+const  (
+	URL = "127.0.0.1:27017"
+	MAX_DISTANCE = 20000000
+)
 
 func  set(uid string, longitude float64, latitude float64)  error {
-	session, err := mgo.Dial("127.0.0.1:27017")
+	session, err := mgo.Dial(URL)
 	if err != nil {
 		panic(err)
 	}
-	defer session.Close() 
+	defer session.Close()
 
 	session.SetMode(mgo.Monotonic, true)
 
-	c := session.DB("test").C("location")
+	mgo.SetDebug(true)
+	var aLogger *log.Logger
+	aLogger = log.New(os.Stderr, "", log.LstdFlags)
+	mgo.SetLogger(aLogger)
 
-	fmt.Println(uid);
-	fmt.Println(longitude);
-	fmt.Println(latitude);
+
+	c := session.DB("test").C("location")
 
 	m := map[string]interface{}{ "loc": bson.M{"type":"Point", "coordinates": []float64{longitude, latitude}}, "uid": uid}
 	err = c.Insert(m)
@@ -41,8 +47,8 @@ func  set(uid string, longitude float64, latitude float64)  error {
 	return err
 }
 
-func  get(uid string)  Location {
-	session, err := mgo.Dial("127.0.0.1:27017")
+func  get(uid string, longitude float64, latitude float64)  []Location {
+	session, err := mgo.Dial(URL)
 	if err != nil {
 		panic(err)
 	}
@@ -50,15 +56,19 @@ func  get(uid string)  Location {
 
 	session.SetMode(mgo.Monotonic, true)
 
+	mgo.SetDebug(true)
+	var aLogger *log.Logger
+	aLogger = log.New(os.Stderr, "", log.LstdFlags)
+	mgo.SetLogger(aLogger)
+
 	c := session.DB("test").C("location")
 
-	fmt.Println(uid);
-
-	var result Location
-	err = c.Find(bson.M{"uid":uid}).One(&result)
+	var result []Location
+	m := map[string]interface{}{ "loc" : bson.M{ "$nearSphere" : bson.M{ "$geometry" : bson.M{ "type" : "Point", "coordinates" : []float64{longitude, latitude}, "$maxDistance": MAX_DISTANCE}}}}
+	
+	err = c.Find(m).All(&result)
 	if err != nil {
-		panic(err)
+		panic(err)  //can not throw exception
 	}
-	fmt.Println(result);
 	return result
 }
