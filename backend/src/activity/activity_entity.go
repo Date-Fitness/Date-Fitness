f725166f7a991bebf8e06b65dd12d11a8591edad
GrantZheng <zhengyuangrant@126.com>
2015-07-05 15:03:12 +0800

    add activity

---
+++ backend/src/activity/activity_entity.go
@@ -0,0 +1,22 @@
+package activity 
+
+import (
+	"log"
+	"os"
+	"hera"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type ActivityEntity struct {
+}
+
+func (this *ActivityEntity) create() error {
+
+}
+
+func (this *ActivityEntity) get() error {
+
+}
+
+
